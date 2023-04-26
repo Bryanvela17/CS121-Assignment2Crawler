a5beb11f79fa48fdca48507dd4d3644a4248437e
Bryan Vela <velabryan17@gmail.com>
2023-04-26 20:21:15 +0000

    Added functions to get nested urls and reutrn aboslute urls

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urldefrag
 from bs4 import BeautifulSoup  ## This is a library for web crawling html or xlm documents
 import nltk  # Manually Added
 from nltk.corpus import stopwords  # Manually Added
@@ -11,32 +11,21 @@ count_Words = defaultdict(int)
 words_In_Page = {}
 # Extra stop words that aren't in the download
 add_These_Words = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are",
-                   "aren't",
-                   "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but",
-                   "by", "can't",
-                   "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't",
-                   "down", "during",
-                   "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't",
-                   "having", "he",
-                   "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his",
-                   "how", "how's",
-                   "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its",
-                   "itself", "let's",
-                   "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on",
-                   "once", "only", "or",
-                   "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she",
-                   "she'd", "she'll",
-                   "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the",
-                   "their", "theirs", "them",
-                   "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're",
-                   "they've", "this", "those",
-                   "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd",
-                   "we'll", "we're", "we've", "were",
-                   "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who",
-                   "who's", "whom", "why", "why's",
-                   "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your",
+                   "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but",
+                   "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't",
+                   "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't",
+                   "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his",
+                   "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its",
+                   "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on",
+                   "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she",
+                   "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the",
+                   "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're",
+                   "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd",
+                   "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who",
+                   "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your",
                    "yours", "yourself", "yourselves"}
 stop_Words = stop_Words.union(add_These_Words)
+
 
 
 def scraper(url, resp):
@@ -58,23 +47,24 @@ def extract_next_links(url, resp):
     listOfLinks = []  # This is where the list of hyperlinks will go
     listOfLinkText = []  # Empty string needed for adding all the words in each url
 
-    print(
-        f'\t\tURL Name ---> : {url}\t\t')  # Should print the url names so that we can see what's going on and to help debug
+    print(f'\t\tURL Name ---> : {url}\t\t')  # Should print the url names so that we can see what's going on and to help debug
 
     if resp.status == 200:  # Checks to see if the status code is valid https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
         if resp.raw_response is not None:
             listOfLinkText = raw_Response(resp)
             counter = 0
             all_Count(listOfLinkText, counter)
-            print(
-                f'\t\tURL Text ---> : {listOfLinkText}\t\t')  # Prints all valid text which can later be used to tuple url with wordset https://www.crummy.com/software/BeautifulSoup/bs4/doc/#get-text
+            print(f'\t\tURL Text ---> : {listOfLinkText}\t\t')  # Prints all valid text which can later be used to tuple url with wordset https://www.crummy.com/software/BeautifulSoup/bs4/doc/#get-text
         else:
             return []
     else:
         return []
-    counter = all_Count(listOfLinkText, counter)
-    words_In_Page[counter] = url
-    # TODO: Begin to parse for urls contained in each url (inception) and make sure they are valid_urls()
+    counter = all_Count(listOfLinkText, counter)            # Gets the number of words per page, and starts tallying all total words
+    words_In_Page[counter] = url                            # Assigns and maps the number of words per page to each specific url
+    listOfLinks = getAllUrls(listOfLinks)                   # Gets all links within a url (recurrsive/inception like behavior)
+    listOfLinks = convertToAbsolute(url, listOfLinks)       # Converts all urls to absolute
+    print(f'\t\tThis URL: {url} has this many words ---> len{listOfLinkText}\t\t')
+    
 
     return listOfLinks
 
@@ -130,3 +120,20 @@ def all_Count(listofLinkText, counter) -> int:
             counter += 1
     return counter
 
+def getAllUrls(listOfLinks) -> list:
+    for everyLink in soup.findAll('a')
+        listOfLinks.append(everyLink.get('href'))
+    return listOfLinks
+
+def convertToAbsolute(url, urlList) -> list[str]:
+    completeUrls = []
+    for link in urlList:
+        complete_link = urljoin(url, link)  # Combines the link with the regular url to get abso link
+        parsedLink = urlparse(complete_link) # Parse the abso link to verify that it's abosulte
+        if parsedLink.scheme:
+            completeUrls.append(complete_link)  # If it's complete just add
+        elif parsedLink.netloc
+            completeUrls.append(f"http{complete_link}") # If it's partial, just add http
+        else:
+            completeUrls.append(f"{url.rstrip('/')}/{complete_link}")   # If it's relative, strip thisLink and add currLink to the end 
+    return completeUrls
