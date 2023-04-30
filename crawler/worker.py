390d73a3e31c6dc32d684cab78eb7057b6851408
Bryan Vela <velabryan17@gmail.com>
2023-04-30 09:48:15 +0000

    Running draft for output, need comments and check for bugs overnight

--- crawler/worker.py
+++ crawler/worker.py
@@ -28,10 +28,12 @@ class Worker(Thread):
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
             resp = download(tbd_url, self.config, self.logger)
+            print(f'---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
             scraped_urls = scraper.scraper(tbd_url, resp)
+            #printCrawlerSummary()
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
